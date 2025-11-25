52c1a819f3270579a97f1df8836d67c00edc66f1
EmilFerdinanA <emilakbar55@gmail.com>
2025-11-25 18:10:34 +0700

    first api ts rest

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import { createExpressEndpoints, initServer } from "@ts-rest/express";
+
+import { pokemonContract } from "./contract.ts";
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Emil Ferdinan Akbar sundel jamak");
+const s = initServer();
+
+const router = s.router(pokemonContract, {
+  getPokemon: async ({ params: { id } }) => {
+    return {
+      status: 200,
+      body: { name: id },
+    };
+  },
 });
+
+createExpressEndpoints(pokemonContract, router, app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
