7657b6c8b1fd4836f9db20746da2da6251ee085d
EmilFerdinanA <emilakbar55@gmail.com>
2025-11-28 20:43:44 +0700

    set cookie after login

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -42,7 +42,7 @@ export const userRouter = s.router(userContract, {
     };
   },
 
-  login: async ({ body }) => {
+  login: async ({ body, req, res }) => {
     const user = await UserModel.findOne({ email: body.email });
     if (!user) {
       return {
@@ -67,6 +67,13 @@ export const userRouter = s.router(userContract, {
 
     const token = jwt.sign({ id: user._id, emaiil: user.email }, "test key", {
       expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return {
